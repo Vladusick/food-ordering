a7bf1404aa910668518d5383395a7746eefb568d
Vladusick <samuelfs@mail.ru>
2026-07-04 01:29:44 +0400

    added static images

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,9 +5,15 @@ import { DishesModule } from './dishes/dishes.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { ConfigService } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+    rootPath: join(__dirname, '..', 'uploads'),
+    serveRoot: '/uploads',
+    }),
     ConfigModule.forRoot({
       isGlobal: true
     }),
